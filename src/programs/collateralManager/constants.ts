3d2282755cfda8ebbb97f357d95299272699f194
Jawad Ahmed <jawad@on-chain.io>
2022-06-16 15:52:38 +0500

    Fixes

--- src/programs/collateralManager/constants.ts
+++ src/programs/collateralManager/constants.ts
@@ -4,7 +4,7 @@ import { PublicKey } from "@solana/web3.js";
 import * as COLLATERAL_MANAGER_TYPES from "../../idl/cardinal_collateral_manager";
 
 export const COLLATERAL_MANAGER_ADDRESS = new PublicKey(
-  "t7vjju88Hj9imjKujtWUJDfRMjsyh9p8Egz6Cw59GA3"
+  "vcmVnNNod8qTzr3n3WDkDSaSckJF9xYVU8hmtdSjWuX"
 );
 
 export const COLLATERAL_MANAGER_SEED = "collateral-manager";
